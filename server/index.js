23c96f8aa4e9752f7e138c0d2b8159b3df3856c8
Lee Stewart <luigizan88@gmail.com>
2020-08-26 14:05:38 +0100

    switch to ftp server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,37 @@
-const path = require('path');
+const FtpSrv = require('ftp-srv');
+const ftpServer = new FtpSrv({
+  url: 'ftp://192.168.1.70:21',
+  pasv_url: 'ftp://192.168.1.70',
+  pasv_min: 8400,
+  pasv_max: 8500,
+  greeting: 'You are connected to PontyPlex File Server!'
+});
 
-let config = {
-  fsRoot: path.resolve('/Users/leestewart/Downloads/'),
-  rootName: 'PontyPlex Library',
-  port: process.env.PORT || '3020',
-  host: process.env.HOST || 'localhost'
-};
-let fileManager = require('@opuscapita/filemanager-server');
-fileManager.server.run(config);
+ftpServer.on('login', ({connection, username, password}, resolve, reject) => {
+  connection.on('RETR', (error, filePath) => {
+    if (error) {
+      console.error(`FTP server error: could not retrieve file ${filePath} for download ${error}`);
+    }
+    console.info(`FTP server: download successfully retrieved - ${filePath}`);
+  });
+  connection.on('STOR', (error, filePath) => {
+    if (error) {
+      console.error(`FTP server error: could not receive file ${filePath} for upload ${error}`);
+    }
+    console.info(`FTP server: upload successfully received - ${filePath}`);
+  });
+  connection.on('RNTO', (error, filePath) => {
+    if (error) {
+      console.error(`FTP server error: could not rename file ${filePath}, error occurred: ${error}`);
+    }
+    console.info(`FTP server: file successfully renamed - ${filePath}`);
+  });
+  resolve({ root: '/Users/leestewart/Downloads'})
+});
+
+ftpServer.on('client-error', ({ context, error }) => {
+  console.error(`FTP server error: error interfacing with client ${context} ${error} ${JSON.stringify(error)}`);
+});
+
+ftpServer.listen()
+  .then(() => { console.log ( `Server running at ftp://192.168.1.70:21`) });
