d95c3d5e4b1832fd288d95267f7dfa5184e02e34
zantetsuken88 <luigizan88@gmail.com>
2021-04-01 13:34:53 +0100

    limit filename length

--- server/utils.js
+++ server/utils.js
@@ -1,5 +1,7 @@
 export const sanitiseFilename = (filename) => {
   let splitName = filename.split('.');
   const ext = splitName.pop();
-  return splitName.join('-').concat(`.${ext}`);
+  let sanitised = splitName.join(' ');
+  sanitised = sanitised.length > 40 && sanitised.substring(0, 40);
+  return sanitised.concat(`.${ext}`);
 }
