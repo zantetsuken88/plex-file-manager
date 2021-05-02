6d201ec271b99d3ca80abad7d181308fd3d5bf8c
zantetsuken88 <luigizan88@gmail.com>
2021-05-02 09:18:01 +0100

    add more logging to filename sanitiser

--- server/utils.js
+++ server/utils.js
@@ -1,11 +1,19 @@
 import { terminalColours } from './terminal-colours.js';
 
 export const sanitiseFilename = (filename) => {
-  let splitName = filename.split('.');
-  const ext = splitName.pop();
-  let sanitised = splitName.length > 1 ? splitName.join(' ') : splitName.toString();
-  sanitised = sanitised.length > 40 ? sanitised.substring(0, 40) : sanitised;
-  return sanitised.concat(`.${ext}`);
+  const { cyan } = terminalColours;
+  try {
+    printColour(cyan, `trying to sanitise ${filename}`);
+    let splitName = filename.split('.');
+    const ext = splitName.pop();
+    printColour(cyan, `extension is ${ext}`);
+    let sanitised = splitName.length > 1 ? splitName.join(' ') : splitName.toString();
+    sanitised = sanitised.length > 40 ? sanitised.substring(0, 40) : sanitised;
+    printColour(cyan, `sanitised filename is "${sanitised}" of type [${typeof sanitised}]`);
+    return sanitised.concat(`.${ext}`);
+  } catch (err) {
+    console.error(`encountered error sanitising filename. filename: ${filename}`);
+  }
 }
 
 export const printColour = (colour, text) => process.stdout.write(colour + text + terminalColours.reset + '\n');
